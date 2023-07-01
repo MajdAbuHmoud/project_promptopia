4095d5a9dbb6aae3d907d83a59594510ec181715
Majd Abu Hmoud <majd.sami1996@gmail.com>
2023-07-01 04:53:58 +0300

    removing console log

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -57,10 +57,6 @@ function Feed() {
     const getPrompts = async () => {
       const getPromptsData = await fetch("/api/prompt");
       const promptsData = await getPromptsData.json();
-      console.log(
-        "🚀 ~ file: Feed.tsx:60 ~ getPrompts ~ promptsData:",
-        promptsData
-      );
       setPosts(promptsData);
     };
     getPrompts();
